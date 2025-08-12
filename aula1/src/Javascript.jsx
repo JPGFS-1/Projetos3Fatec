92de983c88d0d928577cc1652cbe45a4f2c39375
JPGFS <jpgfs8@gmail.com>
2025-08-12 17:54:09 -0300

    Atualização

--- aula1/src/Javascript.jsx
+++ aula1/src/Javascript.jsx
@@ -1,10 +1,15 @@
 function Javascript() {
-
   const name = 'JPGFS';
+  let nome = 'JPGFS';
+
+  const soma = (a, b) => a + b;
 
   return (
     <div>
       <h1>{name}</h1>
+      <h1>{nome}</h1>
+      <h1>A soma é: {2 + 2}</h1>
+      <h1>A soma é: {soma(1000, 1000)}</h1>
     </div>
   )
 }
